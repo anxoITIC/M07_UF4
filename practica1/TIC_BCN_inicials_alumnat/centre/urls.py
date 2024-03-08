ac23fec0a3e26daa6e4b6787edfbd1c7e018c1f3
anxoaragundi <anxoaragundimesias21@gmail.com>
2024-03-08 17:28:17 +0100

    dades de profes i alumnes fetes i templates dels dos fetes

--- practica1/TIC_BCN_inicials_alumnat/centre/urls.py
+++ practica1/TIC_BCN_inicials_alumnat/centre/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = {
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('prof', views.proff, name='proff'),
+    path('users', views.users, name='users')
 }
--- practica1/TIC_BCN_inicials_alumnat/centre/views.py
+++ practica1/TIC_BCN_inicials_alumnat/centre/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.template import Context, loader
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    template = loader.get_template("index.html")
-    return HttpResponse("Visca el Barça")
+    professor = {"name":"Roger", "surname":"Sobrino", "age":"42"}
+
+    return render(request, 'index_centre.html', {'nombre':professor["name"], 'surname':professor["surname"], 'age':professor["age"]})
+
+
+def proff(request):
+    prof = {"id":"1", "name":"Roger", "surname":"Sobrino", "age":"42", "rol":"m07", "curs":"primer"}
+    prof = {"id":"2", "name":"Lionel", "surname":"Messi", "age":"36", "rol":"Barça", "curs":"primer"}
+    prof = {"id":"3", "name":"Juanma", "surname":"No disponible", "age":"53", "rol":"m06", "curs":"primer"}
+    return render(request, 'proff.html', {'id':prof["id"], 'nom':prof["name"], 'cognom':prof["surname"], 'edat':prof["age"], 'rol':prof["rol"], 'curs':prof["curs"]})
+
+def users(request):
+    alumn = {"id":"1", "name":"Anxo", "surname":"Aragundi", "age":"20", "rol":"atractiu", "curs":"primer"}
+    alumn = {"id":"2", "name":"Carlos Andrés", "surname":"Zambrano", "age":"24", "rol":"cregut", "curs":"primer"}
+    alumn = {"id":"3", "name":"Eric", "surname":"Sánchez", "age":"19", "rol":"no fa gràcia", "curs":"primer"}
+    alumn = {"id":"4", "name":"Joel", "surname":"Ghanem", "age":"19", "rol":"pesat", "curs":"primer"}
+    return render(request, 'users.html', {'id':alumn["id"], 'nom':alumn["name"], 'cognom':alumn["surname"], 'edat':alumn["age"], 'rol':alumn["rol"], 'curs':alumn["curs"]})
