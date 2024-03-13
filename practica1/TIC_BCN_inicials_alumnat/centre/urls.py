a0da8fdd839db27de3db686973faaaf11d5fd7e5
anxoaragundi <anxoaragundimesias21@gmail.com>
2024-03-13 20:42:49 +0100

    practica acabada i crec que el readme tambe esta tot fet

--- practica1/TIC_BCN_inicials_alumnat/centre/urls.py
+++ practica1/TIC_BCN_inicials_alumnat/centre/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('prof', views.proff, name='proff'),
-    path('users', views.users, name='users')
+    path('users', views.users, name='users'),
+    path('info_user/<int:user_id>/', views.info_user, name='info_user'),
+    path('info_prof/<int:professor_id>/', views.info_prof, name='info_prof')
 ]
