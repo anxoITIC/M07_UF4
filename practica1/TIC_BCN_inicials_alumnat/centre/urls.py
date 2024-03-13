265866f8ab3b1aad892602197f4875d8f5cf3c60
anxoaragundi <anxoaragundimesias21@gmail.com>
2024-03-13 18:30:42 +0100

    ja es mostra la informació correctament i hi ha header funcional, falta el botó d'info

--- practica1/TIC_BCN_inicials_alumnat/centre/urls.py
+++ practica1/TIC_BCN_inicials_alumnat/centre/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.index, name='index'),
     path('prof', views.proff, name='proff'),
     path('users', views.users, name='users')
-}
+]
--- practica1/TIC_BCN_inicials_alumnat/centre/views.py
+++ practica1/TIC_BCN_inicials_alumnat/centre/views.py
@@ -11,17 +11,23 @@ def index(request):
 
 def proff(request):
     professors = [
-        {"id":"1", "name":"Roger", "surname":"Sobrino", "age":"42", "rol":"m07", "curs":"primer"},
-        {"id":"2", "name":"Lionel", "surname":"Messi", "age":"36", "rol":"Barça", "curs":"primer"},
-        {"id":"3", "name":"Juanma", "surname":"No disponible", "age":"53", "rol":"m06", "curs":"primer"}
+        {"id":"1", "name":"Roger", "surname":"Sobrino", "age":"42", "rol":"m07", "curs":"DAW2"},
+        {"id":"2", "name":"Lionel", "surname":"Messi", "age":"36", "rol":"Barça", "curs":"DAW2"},
+        {"id":"3", "name":"Juanma", "surname":"No disponible", "age":"53", "rol":"m06", "curs":"DAW2"},
+        {"id":"4", "name":"Leonardo", "surname":"Di Caprio", "age":"53", "rol":"m06", "curs":"DAW2"}
+
     ]
     return render(request, 'proff.html', {'professors': professors})
 
 def users(request):
-    students = [
-        {"id":"1", "name":"Anxo", "surname":"Aragundi", "age":"20", "rol":"atractiu", "curs":"primer"},
-        {"id":"2", "name":"Carlos Andrés", "surname":"Zambrano", "age":"24", "rol":"cregut", "curs":"primer"},
-        {"id":"3", "name":"Eric", "surname":"Sánchez", "age":"19", "rol":"no fa gràcia", "curs":"primer"},
-        {"id":"4", "name":"Joel", "surname":"Ghanem", "age":"19", "rol":"pesat", "curs":"primer"}
+    alumne = [
+        {"id":"1", "name":"Anxo", "surname":"Aragundi", "age":"20", "rol":"atractiu", "curs":"DAW2"},
+        {"id":"2", "name":"Carlos Andrés", "surname":"Zambrano", "age":"24", "rol":"cregut", "curs":"DAW2"},
+        {"id":"3", "name":"Eric", "surname":"Sánchez", "age":"19", "rol":"no fa gràcia", "curs":"DAW2"},
+        {"id":"4", "name":"Joel", "surname":"Ghanem", "age":"19", "rol":"pesat", "curs":"DAW2"},
+        {"id":"5", "name":"Jun", "surname":"No el se escriure", "age":"20", "rol":"estudiant", "curs":"DAW2"},
+        {"id":"6", "name":"Lamine", "surname":"Yamal", "age":"16", "rol":"baller", "curs":"DAW2"},
+        {"id":"7", "name":"Jules", "surname":"Koundé", "age":"24", "rol":"flow", "curs":"DAW2"}
+
     ]
-    return render(request, 'users.html', {'students': students})
+    return render(request, 'users.html', {'alumne': alumne})
