2006aaa1859ce0364fd0a2d2db3f87ceca7616eb
anxoaragundi <anxoaragundimesias21@gmail.com>
2024-03-24 22:55:05 +0100

    practica3 feta

--- practica1/TIC_BCN_inicials_alumnat/centre/urls.py
+++ practica1/TIC_BCN_inicials_alumnat/centre/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+
+#differents rutes de la aplicació
 urlpatterns = [
     path('', views.index, name='index'),
     path('prof', views.proff, name='proff'),
     path('users', views.users, name='users'),
     path('info_user/<int:user_id>/', views.info_user, name='info_user'),
-    path('info_prof/<int:professor_id>/', views.info_prof, name='info_prof')
+    path('info_prof/<int:professor_id>/', views.info_prof, name='info_prof'),
+    path('formulari', views.formulari, name='formulari')
 ]
